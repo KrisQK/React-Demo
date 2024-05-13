ec4e7675f478a1c43062de40d3429f3cd509775f
Kris Qikun Li - Mac <qikunli8866+mac@gmail.com>
2024-05-13 12:24:43 +1200

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,31 @@
-import Card from "./Card";
+import Student from "./Student.jsx";
 function App() {
-  return (
-    <>
-      <Card />;
-      <Card />;
-      <Card />;
-      <Card />;
-      <Card />;
-    </>
-  );
+    return (
+        <>
+            <Student
+                name="Spongebob"
+                age="30"
+                isStudent={true}
+            />
+            <Student
+                name="Patrick"
+                age={25}
+                isStudent={false}
+            />
+            <Student
+                name="Squidward"
+                age={20}
+                isStudent={false}
+            />
+
+            <Student
+                name="Sandy"
+                age={28}
+                isStudent={true}
+            />
+            <Student />
+            <Student />
+        </>
+    );
 }
 export default App;
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,14 @@
+const Button = () => {
+    const styles = {
+        backgroundColor: "aqua",
+        color: "white",
+        padding: "10px 20px",
+        border: "none",
+        borderRadius: "5px",
+        fontSize: "16px",
+        cursor: "pointer",
+    };
+    return <button style={styles}>Button</button>;
+};
+
+export default Button;
